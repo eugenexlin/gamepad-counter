99d3853a1fe14664f1c5941fa0a62b3a54c63cb8
Eugene Lin <eugenexlin@gmail.com>
2021-06-14 03:54:44 -0700

    fixed some flashing css on first load problem
    fixed using gatsby-plugin-material-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,18 @@ module.exports = {
     siteMetadata: {
         title: "gamepad counter",
     },
-    plugins: ["gatsby-plugin-sass", "gatsby-plugin-typescript"],
-
+    plugins: [
+        "gatsby-plugin-sass",
+        "gatsby-plugin-typescript",
+        {
+          resolve: 'gatsby-plugin-material-ui',
+          // If you want to use styled components you should change the injection order.
+          options: {
+            // stylesProvider: {
+            //   injectFirst: true,
+            // },
+          },
+        },
+    ],
     pathPrefix: "/gamepad-counter",
 };
